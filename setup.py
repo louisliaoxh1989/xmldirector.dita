414e242fdbcb8cc9442c0476bff0dc3511714ecb
Andreas Jung <info@zopyx.com>
2016-06-21 18:54:07 +0200

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.0.16'
+version = '0.0.17'
 
 long_description = \
     open(os.path.join("docs", "source", "README.rst")).read() + "\n" + \
