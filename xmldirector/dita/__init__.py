41f1770e2fa5b9e92c7caa65b40a1689f372d146
Andreas JungÂ <info@zopyx.com>
2016-06-22 10:31:21 +0200

    added h2d.xsl

--- xmldirector/dita/__init__.py
+++ xmldirector/dita/__init__.py
@@ -1,1 +1,19 @@
 # placeholder
+
+try:
+    import xmldirector.plonecore
+    have_xmldirector = True
+except ImportError:
+    have_xmldirector = False
+
+if have_xmldirector:
+
+    import os
+    from xmldirector.plonecore.transformer_registry import TransformerRegistryUtility
+    from xmldirector.plonecore.validator_registry import ValidatorRegistryUtility
+
+    cwd = os.path.dirname(__file__)
+
+    TransformerRegistryUtility.register_transformation(
+        'dita', 'html2dita', os.path.join(cwd, 'converters', 'h2d', 'h2d.xsl'))
+
