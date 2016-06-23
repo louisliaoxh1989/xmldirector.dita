44c6655de778c5937ee57480677d6e513d270aac
Andreas JungÂ <info@zopyx.com>
2016-06-23 13:04:11 +0200

    updated

---
+++ xmldirector/dita/browser/map_editor.py
@@ -0,0 +1,13 @@
+
+################################################################
+# xmldirector.dita
+# (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+from Products.Five.browser import BrowserView
+
+
+class MapEditor(BrowserView):
+
+    pass
